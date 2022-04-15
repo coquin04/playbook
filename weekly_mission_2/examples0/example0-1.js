1351836c9f554d7297718f066e07f0f24eb9a177
jorge-juareza <coquin04@gmail.com>
2022-04-15 12:16:00 -0500

    Create: Dir for examples 0 and examples0-1.js

---
+++ weekly_mission_2/examples0/example0-1.js
@@ -0,0 +1,5 @@
+console.log("Objeto vacio");
+// Ejemplo 1: Crear un objeto
+const myObjetc = {}; // Esto es un objeto vacío
+console.log("Ejemplo 1: Crear un objeto vacío");
+console.log(myObjetc);
