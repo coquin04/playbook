c60b8572ef3bd0d7e28d56b61ac16b518182e5f2
jorge-juareza <coquin04@gmail.com>
2022-04-18 18:42:17 -0500

    Fix: file package.json.

--- weekly_mission_2/examples4/pokemon.test.js
+++ weekly_mission_2/examples4/pokemon.test.js
@@ -1,6 +1,6 @@
-import { Pokemon } from "";
+import Pokemon from "./pokemon";
 
 test("1) Create a new object pokemon", () => {
   const myPokemon = new Pokemon("Pikachu");
-  expect(myPokemon.name).toBe("Pikachussssssss"); // Corrige esta prueba
+  expect(myPokemon.name).toBe("Pikachu"); // Corrige esta prueba
 });
