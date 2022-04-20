11f9c965daa0a4bf82114696f1924229a7f4c905
jorge-juareza <coquin04@gmail.com>
2022-04-19 23:36:43 -0500

    Add: missionCommander test and end of project 1.

---
+++ weekly_mission_3/my_launchx_app/test/missionCommander.test.js
@@ -0,0 +1,8 @@
+const MissionCommander = require("./../app/missionCommander");
+
+describe("Unit test for mission commander Class", () => {
+  test("1) Create a mission commander object", () => {
+    const myMissionCommander = new MissionCommander("Woopa");
+    expect(myMissionCommander.name).toBe("Woopa");
+  });
+});
