cdf336f1486c9100892234e883fea19a216ee5ef
jorge-juareza <coquin04@gmail.com>
2022-04-22 21:37:39 -0500

    Add: Test for requirement 2 from 4_sprint_2...

--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -7,4 +7,12 @@ describe("Tests for UserService class", () => {
     expect(user.id).toBe(1);
     expect(user.bio).not.toBeUndefined();
   });
+  test("2. Get all user data in a list", () => {
+    const user = UserService.create(1, "carlogilmar", "Carlo");
+    const userInfoInList = UserService.getInfo(user);
+    expect(userInfoInList[0]).toBe("carlogilmar");
+    expect(userInfoInList[1]).toBe("Carlo");
+    expect(userInfoInList[2]).toBe(1);
+    expect(userInfoInList[3]).not.toBeUndefined();
+  });
 });
