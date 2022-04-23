fa02af80cd59eb3d22bcf2f5ef8e1b4602788890
jorge-juareza <coquin04@gmail.com>
2022-04-22 21:38:29 -0500

    Add: getInfo to pass test from requirement 2

--- weekly_mission_3/twitter/app/services/UserService.js
+++ weekly_mission_3/twitter/app/services/UserService.js
@@ -4,5 +4,9 @@ class UserService {
   static create(id, username, name) {
     return new User(id, username, name, "Sin bio");
   }
+  // metodo getInfo
+  static getInfo(user) {
+    return [user.username, user.name, user.id, user.bio];
+  }
 }
 module.exports = UserService;
