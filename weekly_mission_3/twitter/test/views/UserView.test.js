27edbbfb309be205d850b9f228131b7e7745b993
jorge-juareza <coquin04@gmail.com>
2022-04-24 22:21:42 -0500

    Add: result object.

--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -8,6 +8,7 @@ describe("Tests for UserView class", () => {
   });
   test("2. Return an error object when try to create a new user with a payload with invalid properties", () => {
     const payload = { username: null, name: 12, id: "id" };
+    const result = UserView.createUser(payload);
     expect(result.error).toMatch(/necesitan tener un valor valido/);
   });
 });
