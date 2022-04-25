7fc461260f6c334a297a9a772210ea54b56c513c
jorge-juareza <coquin04@gmail.com>
2022-04-24 21:58:49 -0500

    Add: Test for requirement 2 from Sprint 3.

--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -1,9 +1,13 @@
 const UserView = require("./../../app/views/UserView");
 
 describe("Tests for UserView class", () => {
-  test("Return an error object when try to create a new user with a null payload", () => {
+  test("1. Return an error object when try to create a new user with a null payload", () => {
     const payload = null;
     const result = UserView.createUser(payload);
     expect(result.error).toMatch(/payload no existe/);
+  });
+  test("2. Return an error object when try to create a new user with a payload with invalid properties", () => {
+    const payload = { username: null, name: 12, id: "id" };
+    expect(result.error).toMatch(/necesitan tener un valor valido/);
   });
 });
