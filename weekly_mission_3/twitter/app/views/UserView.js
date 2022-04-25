f3baec15dc0416749824f44b563f9d8383ec5155
jorge-juareza <coquin04@gmail.com>
2022-04-24 22:53:12 -0500

    Update: UserView.js to pass test 3 from requirement 3.

--- weekly_mission_3/twitter/app/views/UserView.js
+++ weekly_mission_3/twitter/app/views/UserView.js
@@ -2,9 +2,16 @@ const UserService = require("./../services/UserService");
 
 class UserView {
   static createUser(payload) {
+    // Para pasar el primer test
     if (payload === null) {
       return { error: "payload no existe" };
+      // Para pasar el segundo test
     } else if (Object.values(payload).some((value) => value === null)) {
+      return { error: "necesitan tener un valor valido" };
+      // Para pasar el tercer test
+    } else if (
+      !["username", "name", "id"].every((key) => key in Object.keys(payload))
+    ) {
       return { error: "necesitan tener un valor valido" };
     } else {
       return UserService.create(
