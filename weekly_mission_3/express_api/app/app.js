cc0716c82bd361bf472012d1af59b7cd28635223
jorge-juareza <coquin04@gmail.com>
2022-04-29 21:09:02 -0500

    Update: PUT from 7 express api.

--- weekly_mission_3/express_api/app/app.js
+++ weekly_mission_3/express_api/app/app.js
@@ -29,6 +29,13 @@ app.post("/v1/explorers", (req, res) => {
   const requestBody = req.body; //Parametros de un cliente
   res.status(201).json({ message: "Created" });
 });
+// PUT Crea un endpoint que se encargue de actualizar un explorer
+app.put("/v1/explorers/:id", (req, res) => {
+  console.log(`Api Explorers PUT request ${new Date()}`);
+  console.log(`Update explorer with id ${req.params.id}`);
+  const requestBody = req.body; // Parametros de un cliente
+  res.status(200).json({ message: "Updated!" });
+});
 // Con esto inicializamos esta app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
