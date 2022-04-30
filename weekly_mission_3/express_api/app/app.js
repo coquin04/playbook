d4f1ca95b39cc8e093d017cef71f81ea98ad6bd9
jorge-juareza <coquin04@gmail.com>
2022-04-29 21:01:08 -0500

    Update: GET Crea un endpoint que regrese un explorer mediando ID.

--- weekly_mission_3/express_api/app/app.js
+++ weekly_mission_3/express_api/app/app.js
@@ -6,6 +6,7 @@ app.use(express.json()); // Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000;
 // HTTP Methods
+// GET Crea un endpoint que regrese una lista de explorers
 app.get("/v1/explorers", (req, res) => {
   console.log(`Api Explorers GET ALL request ${new Date()}`);
   const explorer1 = { id: 1, name: "Carlo1" };
@@ -14,6 +15,13 @@ app.get("/v1/explorers", (req, res) => {
   const explorer4 = { id: 4, name: "Carlo4" };
   const explorers = [explorer1, explorer2, explorer3, explorer4];
   res.status(200).json(explorers);
+});
+// GET Crea un endpoint que te regrese un explorer mediando un ID
+app.get("/v1/explorers/:id", (req, res) => {
+  console.log(`Api Explorers GET request ${new Date()}`);
+  console.log(`Getting explorer with id ${req.params.id}`);
+  const explorer = { id: 1, name: "Carlo" };
+  res.status(200).json(explorer);
 });
 // Con esto inicializamos esta app
 app.listen(port, () => {
