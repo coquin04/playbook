b6adbf0ca1421dcb4ba3ab16600784707df92de7
jorge-juareza <coquin04@gmail.com>
2022-05-01 22:47:25 -0500

    Update: index.js to use FizzBuzz service.

--- weekly_mission_4/FizzBuzz/index.js
+++ weekly_mission_4/FizzBuzz/index.js
@@ -1,4 +1,5 @@
 // Clase anterior con la que obtenemos los explorers
+const FizzbuzzService = require("./lib/services/FizzbuzzService");
 const ExplorerService = require("./lib/services/ExplorerServices");
 const Reader = require("./lib/utils/Reader");
 const explorers = Reader.readJsonFile("./data/explorers.json");
@@ -7,3 +8,15 @@ const explorers = Reader.readJsonFile("./data/explorers.json");
 ExplorerService.filterByMission(explorers, "node");
 ExplorerService.getAmountOfExplorersByMission(explorers, "node");
 ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+const explorer1 = { name: "Explorer1", score: 1 };
+FizzbuzzService.applyValidationInExplorer(explorer1); // {name: "Explorer1", score: 1, trick: 1}
+
+const explorer3 = { name: "Explorer3", score: 3 };
+FizzbuzzService.applyValidationInExplorer(explorer3); // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = { name: "Explorer5", score: 5 };
+FizzbuzzService.applyValidationInExplorer(explorer5); // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = { name: "Explorer15", score: 15 };
+console.log(FizzbuzzService.applyValidationInExplorer(explorer15));
+// {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
